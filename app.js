0f87831cd917ec2bb6398e28a478aabb4812f2cc
InhongLee <amaco78@gmail.com>
2021-01-31 10:41:32 +0900

    github_link_test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World. updated msg.');
+    res.send('Hello World. updated msg. github linked.');
 });
 
 module.exports = app;
